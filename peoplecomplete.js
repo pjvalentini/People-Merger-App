38f1817864e15d1f5e06a2acb6dc28c586d4edf5
pjvalentini <pj.valentini@gmail.com>
2017-10-25 16:04:01 -0400

    JS Updates and more comments added

--- peoplecomplete.js
+++ peoplecomplete.js
@@ -1,12 +1,16 @@
 "use strict";
 
+// requiring fs
 const fs = require("fs");
 
+// Set up global variables
 const fileOne = require("./people1.json");
 const fileTwo = require("./people2.json");
 const completeFile = 'peopleComplete.txt';
 
-
+// this function has two fs.readFiles with cb functions.
+// that reads fileOne, fileTwo, and holds two local variables
+// that parses the JSON files.
 function parseReadMergeSort(fileOne, fileTwo) {
 	fs.readFile(fileOne, 'utf-8', function(err, data1) {
 		if (err) throw err;
@@ -20,10 +24,14 @@ function parseReadMergeSort(fileOne, fileTwo) {
     	// Alphabetically sorts the items in the array.
   		console.log(merge.sort());
 
+// this fs.writeFile takes in the file_to_write_to...a merge.sort() arg,
+// the text type, and callback function.
 			fs.writeFile('./peopleComplete.txt', merge.sort(), "utf-8", function(err) {
 				if (err) throw err;
 			});
 		});
 	});
 }
+// Finally calling the function that takes the 2 JSON files task in and
+// reads, parses, merges, and sorts all in one function...
 parseReadMergeSort("./people1.json", "./people2.json");
